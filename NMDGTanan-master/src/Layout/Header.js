d1e286fadfa6f261b5659842e7315c1f7a822b41
SecondKnife <96180954+SecondKnife@users.noreply.github.com>
2022-05-05 21:18:54 +0700

    Add files via upload

---
+++ NMDGTanan-master/src/Layout/Header.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import Lichtruc from '../Component/Lichtruc'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-sm navbar-light bg-light.bg-gradient">
+                    <div className="container-fluid">
+                        <a className="navbar-brand"> <i className="fas fa-fan"></i>Nhà máy Điện gió Tân Ân 1</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
+                            aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <button className="btn btn-primary" >
+                        <Link to="/Danhsach" style={{color: 'inherit', textDecoration: 'none'}}>
+                            Danh sach
+                        </Link>
+                        </button>
+                        {/* <a href="/Lichtruc">Lịch trực </a>
+                        <a href="/">Chấm công </a> */}
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+}
+
